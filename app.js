d930810360fcb75dc5aba3a294844e58a993a861
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-03-29 11:31:22 +0200

    create user choice

---
+++ app.js
@@ -0,0 +1,10 @@
+const computerChoiceDisplay = document.querySelector('#computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.querySelector('#result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
+    userChoice = event.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+}))
