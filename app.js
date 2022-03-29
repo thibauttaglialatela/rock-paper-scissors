7a3ceb169a47564ac45fdf8a55c0e666e00b203e
thibauttaglialatela <thibauttaglialatela@gmail.com>
2022-03-29 11:55:13 +0200

    generate computer choice

--- app.js
+++ app.js
@@ -3,8 +3,26 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.querySelector('#result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
+let computerChoice;
+
+const generateComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() *3) + 1;
+    switch (randomNumber) {
+        case 1:
+            computerChoice = 'Rock';
+            break;
+        case 2:
+            computerChoice = 'Paper';
+            break;
+        case 3:
+            computerChoice = 'Scissors';
+            break;
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
     userChoice = event.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
 }))
